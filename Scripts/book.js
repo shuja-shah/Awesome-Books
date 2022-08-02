d0e3201eea3bbc02623e290e9ee6bb327382626c
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-08-02 09:31:16 -0700

    Implement Clear Fields Method

--- Scripts/book.js
+++ Scripts/book.js
@@ -23,14 +23,14 @@ class userInterface{
       beAppended.className = 'kitab';
       beAppended.innerHTML = `
        <h2>${book.title}</h2> <span class = 'creator'>${book.author}</span>
-       <p>${book.id}</p> 
+       <p class="identity-book">${book.id}</p> 
        <button class = 'delete'>Delete</button>
        `;
       getAppended.appendChild(beAppended);
     } 
     static clearFields() {
-      document.getElementById('title').value += '';
-      document.getElementById('author').value += '';
+      document.getElementById('title').value = '';
+      document.getElementById('author').value = '';
     }
 }
   class storage{
@@ -62,24 +62,19 @@ class userInterface{
 
   document.addEventListener('DOMContentLoaded', userInterface.displayBooks);
 
-  //Now we utilize displaybooks methods to display added books.
-
   document.getElementById('form').addEventListener('submit', (e) => {
     const title = document.getElementById('title').value;
     const author = document.getElementById('author').value;
     let id = Math.floor(Math.random() * 1000) + 1;
     id.toString();
     e.preventDefault();
-    // Now we use our constructor from the book class.
     const newBook = new awesomeBooks(title, author ,id);
-    
-    // Now lets add this to our UserInterface.
     userInterface.addBookToList(newBook);
     userInterface.clearFields();
     storage.addition(newBook);
   });
 
-  // Now we add a delete button to our list.
+
   getAppended.addEventListener('click', (e) => {
     if (e.target.classList.contains('delete')) {
       e.target.parentElement.remove();
