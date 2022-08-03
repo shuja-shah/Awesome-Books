3e6c0df118b9a336b8f348d347ececf1b5dcae0c
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-08-03 01:50:18 -0700

    Configure Linter

--- Scripts/book.js
+++ Scripts/book.js
@@ -1,4 +1,7 @@
-const getAppended = document.getElementById("book-container");
+/* eslint-disable max-classes-per-file */
+/* eslint-disable new-cap */
+/* eslint-disable radix */
+const getAppended = document.getElementById('book-container');
 
 class awesomeBooks {
   constructor(title, author, id) {
@@ -10,16 +13,17 @@ class awesomeBooks {
 
 class userInterface {
   static displayBooks() {
-    let books = storage.getData();
+    const books = storage.getData();
 
     const myBooks = books;
     myBooks.forEach((book) => {
       userInterface.addBookToList(book);
     });
   }
+
   static addBookToList(book) {
-    const beAppended = document.createElement("div");
-    beAppended.className = "kitab";
+    const beAppended = document.createElement('div');
+    beAppended.className = 'kitab';
     beAppended.innerHTML = `
        <h4>${book.title}</h4> <span class = 'creator'>${book.author}</span>
        <p class="identity-book">${book.id}</p> 
@@ -27,26 +31,29 @@ class userInterface {
        `;
     getAppended.appendChild(beAppended);
   }
+
   static clearFields() {
-    document.getElementById("title").value = "";
-    document.getElementById("author").value = "";
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
   }
 }
 class storage {
   static getData() {
     let books;
-    if (localStorage.getItem("books") === null) {
+    if (localStorage.getItem('books') === null) {
       books = [];
     } else {
-      books = JSON.parse(localStorage.getItem("books"));
+      books = JSON.parse(localStorage.getItem('books'));
     }
     return books;
   }
+
   static addition(book) {
     const books = storage.getData();
     books.push(book);
-    localStorage.setItem("books", JSON.stringify(books));
+    localStorage.setItem('books', JSON.stringify(books));
   }
+
   static remove(id) {
     const books = storage.getData();
     books.forEach((book, index) => {
@@ -54,16 +61,16 @@ class storage {
         books.splice(index, 1);
       }
     });
-    localStorage.setItem("books", JSON.stringify(books));
+    localStorage.setItem('books', JSON.stringify(books));
   }
 }
 
-document.addEventListener("DOMContentLoaded", userInterface.displayBooks);
+document.addEventListener('DOMContentLoaded', userInterface.displayBooks);
 
-document.getElementById("form").addEventListener("submit", (e) => {
-  const title = document.getElementById("title").value;
-  const author = document.getElementById("author").value;
-  let id = Math.floor(Math.random() * 1000) + 1;
+document.getElementById('form').addEventListener('submit', (e) => {
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const id = Math.floor(Math.random() * 1000) + 1;
   id.toString();
   e.preventDefault();
   const newBook = new awesomeBooks(title, author, id);
@@ -72,8 +79,8 @@ document.getElementById("form").addEventListener("submit", (e) => {
   storage.addition(newBook);
 });
 
-getAppended.addEventListener("click", (e) => {
-  if (e.target.classList.contains("delete")) {
+getAppended.addEventListener('click', (e) => {
+  if (e.target.classList.contains('delete')) {
     e.target.parentElement.remove();
   }
 
